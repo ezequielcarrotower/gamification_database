8b7f6309de3a667fc9d0b57399c6614e5d98dc18
Ezequiel <ezequiel.carro@towerhousestudio.com>
2015-10-02 19:56:44 -0300

    first commit

---
+++ 2014_10_12_000000_create_player_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use towerhouses\database;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('player');
+    }
+}
